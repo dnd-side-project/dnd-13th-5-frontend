4c39a2643f39d76d7a1cc6a6a36f31037a11e2c7
Sungyeon Jung <yeonnybus@gmail.com>
2025-07-24 15:42:55 +0900

    feat: update commitlint config to ES module format

---
+++ commitlint.config.mjs
@@ -0,0 +1,23 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  // rules: {
+  //   'type-enum': [
+  //     2,
+  //     'always',
+  //     [
+  //       'ci',
+  //       'chore',
+  //       'rename',
+  //       'remove',
+  //       'feat',
+  //       'fix',
+  //       'perf',
+  //       'refactor',
+  //       'build',
+  //       'design',
+  //       'docs',
+  //       'hotfix',
+  //     ],
+  //   ],
+  // },
+};
