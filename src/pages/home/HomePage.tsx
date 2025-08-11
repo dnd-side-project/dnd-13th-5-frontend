b2c6d977ff34670250f3f53b403d1ca8a9bbd794
Sungyeon Jung <yeonnybus@gmail.com>
2025-08-11 17:12:45 +0900

    feat: add temporary code to home page

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -2,5 +2,10 @@ export const HomePage = () => (
   <div className="bg-blue-400">
     <h1 className="bg-red-400">Home Page</h1>
     <p>Welcome to the application!</p>
+    <p>test code review text</p>
+    <div>test code review text</div>
+    <h5 className="typo-body-s-medium">test code review text</h5>
+    <h3 className="bg-green-200">test code review text</h3>
+    <a href="https://www.naver.com">test code review text</a>
   </div>
 );
