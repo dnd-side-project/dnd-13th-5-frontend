3ba0b5bdedf5a37a5d6cb8a61f94c974c4524a10
Soyeon Lee <pink731p@gmail.com>
2025-08-13 00:18:41 +0900

    fix: 오타 수정

--- src/shared/assets/icons/index.ts
+++ src/shared/assets/icons/index.ts
@@ -9,7 +9,7 @@ import CloseIcon from './close.svg?react';
 import ComparisonIcon from './comparison.svg?react';
 import HomeIcon from './home.svg?react';
 import PlusIcon from './plus.svg?react';
-import QuestionIcon from './question-circle.svg';
+import QuestionIcon from './question-circle.svg?react';
 import StarIcon from './star.svg?react';
 import SubIcon from './sub.svg?react';
 import UserIcon from './user-profile.svg?react';
