4a44916c2a2b0d2f424047ec2aaa0d2d2d91402b
Soyeon Lee <pink731p@gmail.com>
2025-08-13 18:38:30 +0900

    refactor: ui 역할만 하기 위해 로직 부분 제거

--- src/shared/ui/button/IconButton.tsx
+++ src/shared/ui/button/IconButton.tsx
@@ -1,48 +1,20 @@
 import { Icon, IconProps } from '@/shared/ui/icon';
-import { Link, useNavigate } from 'react-router-dom';
 
 export interface IconButtonProps {
   icon: IconProps;
   ariaLabel: string;
-  href?: string | number;
   onClick?: () => void;
 }
 
-const IconButton = ({
-  icon, ariaLabel, href, onClick,
-}: IconButtonProps) => {
-  const navigate = useNavigate();
-
-  if (typeof href === 'string') {
-    return (
-      <Link
-        to={href}
-        onClick={onClick}
-        aria-label={ariaLabel}
-        className="inline-flex items-center justify-center"
-      >
-        <Icon {...icon} />
-      </Link>
-    );
-  }
-
-  const handleClick = () => {
-    onClick?.();
-    if (typeof href === 'number') {
-      navigate(href);
-    }
-  };
-
-  return (
+const IconButton = ({ icon, ariaLabel, onClick }: IconButtonProps) => (
     <button
       type="button"
-      onClick={handleClick}
+      onClick={onClick}
       aria-label={ariaLabel}
       className="inline-flex items-center justify-center"
     >
       <Icon {...icon} />
     </button>
-  );
-};
+);
 
 export default IconButton;
