2342b4c6080d28a29e7df67162d7e73c68d195c4
Sungyeon Jung <yeonnybus@gmail.com>
2025-08-20 15:19:48 +0900

    fix: type name error

--- src/widgets/payment-summary/model/types.ts
+++ src/widgets/payment-summary/model/types.ts
@@ -4,7 +4,7 @@ export type PaymentSummaryCardProps = {
   title?: string | ReactNode;
   dateText: string;
   iconUrl?: string;
-  serviceNam?: string;
+  serviceName?: string;
   monthUsed: number;
   monthLimit: number;
   progressPercent?: number; // 0~100 범위의 퍼센트 값, 기본값은 0
