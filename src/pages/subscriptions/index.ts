6834dd2a470a2258d1b8fd46b310b35995413624
Soyeon Lee <pink731p@gmail.com>
2025-08-27 02:17:39 +0900

    feat: benefit detail page 구현

--- src/pages/subscriptions/index.ts
+++ src/pages/subscriptions/index.ts
@@ -1,3 +1,4 @@
 export { SubscriptionsPage } from './SubscriptionPage';
 export { SubscriptionDetailPage } from './SubscriptionDetailPage';
+export { BenefitDetailPage } from './BenefitDetailPage';
 export { SubscriptionEditPage } from './SubscriptionEditPage';
