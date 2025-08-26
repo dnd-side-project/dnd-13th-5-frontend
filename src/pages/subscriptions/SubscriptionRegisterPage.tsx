457a06f0005b36309c7b860de361e2e1f57e9960
Sungyeon Jung <yeonnybus@gmail.com>
2025-08-26 16:55:07 +0900

    fix: 사용하지 않는 함수 및 props 정리

--- src/pages/subscriptions/SubscriptionRegisterPage.tsx
+++ src/pages/subscriptions/SubscriptionRegisterPage.tsx
@@ -8,11 +8,6 @@ import { SubscriptionRegisterWidget } from '@/widgets/subscription-register/Subs
 function SubscriptionRegister() {
   const [step, setStep] = useState<1 | 2 | 3 | 4>(1);
 
-  const handleSubmit = async () => {
-    // TODO: 실제 API 호출 로직 구현
-    console.log('구독 등록:');
-  };
-
   return (
     <MobileLayout
       headerProps={{
@@ -26,7 +21,7 @@ function SubscriptionRegister() {
       }}
       showBottom={false}
     >
-      <SubscriptionRegisterWidget step={step} setStep={setStep} onSubmit={handleSubmit} />
+      <SubscriptionRegisterWidget step={step} setStep={setStep} />
     </MobileLayout>
   );
 }
