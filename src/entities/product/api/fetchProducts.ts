a7994f64cea5e3dd1431a657ce4e90e66493a98d
Soyeon Lee <pink731p@gmail.com>
2025-08-26 15:32:15 +0900

    feat: product 관련 api 정의

--- src/entities/product/api/fetchProducts.ts
+++ src/entities/product/api/fetchProducts.ts
@@ -1,19 +1,11 @@
 import apiClient from '@/shared/api/apiClient';
-
-export type CategoryOption =
-  | null
-  | 'OTT'
-  | 'SHOPPING'
-  | 'MUSIC'
-  | 'CLOUD'
-  | 'AI'
-  | 'EDUCATION'
-  | 'DELIVERY'
-  | 'PRODUCTIVITY';
+import type { CategoryOption } from '@/shared/types/category.types';
 
 // API 응답 전체에 대한 타입 (선택적)
 type ProductsResponse = {
-  data: Products; // 실제 데이터는 data 객체 안에 있습니다.
+  data: {
+    products: Products[];
+  }; // 실제 데이터는 data 객체 안에 있습니다.
 };
 
 // 실제 사용자 정보 데이터에 대한 타입
@@ -31,9 +23,9 @@ export type Products = {
  * 이 함수는 오직 API 호출과 핵심 데이터 반환에만 집중합니다.
  * 토큰 주입, 로딩/에러 상태 관리는 각각 apiClient 인터셉터와 React Query가 담당합니다.
  */
-export const fetchProducts = async (query: CategoryOption): Promise<Products> => {
+export const fetchProducts = async (query: CategoryOption): Promise<Products[]> => {
   const response = await apiClient.get<ProductsResponse>('/products', {
     params: { category: query },
   });
-  return response.data.data; // 서버 응답의 data 객체 안의 data를 반환합니다.
+  return response.data.data.products; // 서버 응답의 data 객체 안의 data를 반환합니다.
 };
--- src/entities/product/hook/useProducts.ts
+++ src/entities/product/hook/useProducts.ts
@@ -1,9 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
 
-import type { CategoryOption} from '../api/fetchProducts';
+import type { CategoryOption } from '@/shared/types/category.types';
+
 import { fetchProducts } from '../api/fetchProducts';
 
-export const useProducts = (category: CategoryOption) => useQuery({
+export const useProducts = (category: CategoryOption) =>
+  useQuery({
     queryKey: ['products', category],
     queryFn: () => fetchProducts(category),
     retry: 1, // 토큰 갱신 실패 시 1번만 재시도
