f38adfdee199d0214b442595d51d2e291933e9ab
Soyeon Lee <pink731p@gmail.com>
2025-08-27 02:16:43 +0900

    feat: 비교 섹션의 타입 및 컴포넌트 수정, 추천 서비스 섹션 추가

--- src/widgets/comparison-section/ui/ComparisonMySubSection.tsx
+++ src/widgets/comparison-section/ui/ComparisonMySubSection.tsx
@@ -1,35 +1,31 @@
-import ComparisonCard from '@/widgets/comparison-card/ui/ComparisonCard';
-import type { ComparisonMySubSectionProps } from '@/widgets/comparison-section/model/types';
+// import ComparisonSelectCard from '@/features/comparison-select/ComparisonSelectCard';
+// import type { ComparisonMySubSectionProps } from '@/widgets/comparison-section/model/types';
 
-export const ComparisonMySubSection = ({
-  category,
-  mySubs,
-  selectedSubs,
-  handleSelect,
-  handleDetail,
-}: ComparisonMySubSectionProps) => (
-    <section className="py-[10px] space-y-4 mb-[10px]">
-      <header className="flex typo-body-m-bold">
-        <h2 className="typo-body-m-bold">
-          <span className="text-primary-700">내가 구독 중인</span> {category}
-        </h2>
-      </header>
+// export const ComparisonMySubSection = ({
+//   category,
+//   mySubs,
+//   selectedSubs,
+//   handleSelect,
+//   handleDetail,
+// }: ComparisonMySubSectionProps) => (
+//   <section className="py-[10px] space-y-4 mb-[10px]">
+//     <header className="flex typo-body-m-bold">
+//       <h2 className="typo-body-m-bold">
+//         <span className="text-primary-700">내가 구독 중인</span> {category}
+//       </h2>
+//     </header>
 
-      <div className="grid grid-cols-2 gap-[14px]">
-        {mySubs.map(service => (
-            <ComparisonCard
-              key={service.id}
-              serviceName={service.name}
-              imageUrl={service.imageUrl}
-              minPrice={null}
-              maxPrice={null}
-              isSelected={selectedSubs.includes(service.id)}
-              setIsSelected={() => handleSelect(service.id)}
-              handleDetail={() => handleDetail(service.id)}
-              mySub={true}
-              myPrice={service.price}
-            />
-          ))}
-      </div>
-    </section>
-  );
+//     <div className="grid grid-cols-2 gap-[14px]">
+//       {mySubs.map(p => (
+//         <ComparisonSelectCard
+//           key={p.id}
+//           product={p}
+//           isSelected={selectedSubs.includes(p.id)}
+//           setIsSelected={() => handleSelect(p.id)}
+//               handleDetail={handleDetail}
+//               isMySub={true}
+//         />
+//       ))}
+//     </div>
+//   </section>
+// );
