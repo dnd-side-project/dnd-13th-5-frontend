a97a1c24c663430ab188db1faf83f7f49ef494a0
Soyeon Lee <pink731p@gmail.com>
2025-08-27 22:02:15 +0900

    feat: 홈 페이지 카드 섹션 및 철학 섹션 추가, 이미지 리소스 추가

--- src/shared/assets/images/index.ts
+++ src/shared/assets/images/index.ts
@@ -1,3 +1,4 @@
+import Brain from './brain.png';
 import CategoryAI from './category-ai.webp';
 import CategoryCloud from './category-cloud.webp';
 import CategoryDelivery from './category-delivery.webp';
@@ -7,7 +8,14 @@ import CategoryMusic from './category-music.webp';
 import CategoryOTT from './category-ott.webp';
 import CategoryProductivity from './category-productivity.webp';
 import CategoryShopping from './category-shopping.webp';
+import HomeOne from './home-one.png';
+import HomeThree from './home-three.png';
+import HomeTwo from './home-two.png';
 import Logo from './logo_4x.webp';
+import Money from './money.png';
+import Sad from './sad.png';
+import Scissor from './scissor.png';
+import Smile from './smile.png';
 
 export const CategoryImages = {
   OTT: CategoryOTT,
@@ -22,3 +30,7 @@ export const CategoryImages = {
 };
 
 export { Logo };
+
+export { HomeOne, HomeTwo, HomeThree };
+
+export { Sad, Scissor, Smile, Brain, Money };
